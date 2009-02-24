44f56b156a1e8c8a8f266b53c0cc6f0ea4f97c98
Neutron Soutmun <neo.neutron@gmail.com>
2009-02-25 03:36:03 +0700

    Initial import debian/ into repository

---
+++ debian/examples/config.php
@@ -0,0 +1,21 @@
+<?php
+$config = array(
+  "RADIUS_HOST" => "localhost",
+  "RADIUS_SECRET" => "testing123",
+  "RADIUS_ENCRYPT" => "CHAP_MD5",
+  "RADIUS_AUTH_PORT" => 0,
+  "RADIUS_ACCT_PORT" => 0,
+  "RAHUNAS_HOST" => "localhost",
+  "RAHUNAS_PORT" => "8123",
+  "NAS_IDENTIFIER" => "RahuNAS-01",
+  "NAS_IP_ADDRESS" => "172.30.0.1",
+  "NAS_LOGIN_HOST" => "172.30.0.1",
+  "NAS_LOGIN_PORT" => "8443",
+  "NAS_LOGIN_PROTO" => "https",
+  "NAS_PORT" => 1,
+  "NAS_LOGIN_TITLE" => "RahuNAS Network",
+  "DEFAULT_REDIRECT_URL" => "http://www.kku.ac.th",
+  "DEFAULT_LANGUAGE" => "Thai",
+  "UAM_TEMPLATE" => "rahunas"
+);
+?>
