396e28fcd67965f0ada4c0e1b77482c538197ee1
Neutron Soutmun <neo.neutron@gmail.com>
2012-06-16 21:47:22 +0700

    Correct the definition of task ipset header declaration

--- src/rh-task-ipset.h
+++ src/rh-task-ipset.h
@@ -1,14 +1,14 @@
 /**
- * RahuNAS task memset implementation 
+ * RahuNAS task ipset implementation
  * Author: Neutron Soutmun <neo.neutron@gmail.com>
  * Date:   2008-09-07
  */
-#ifndef __RH_TASK_MEMSET_H
-#define __RH_TASK_MEMSET_H
+#ifndef __RH_TASK_IPSET_H
+#define __RH_TASK_IPSET_H
 
 #include "rh-server.h"
 
 extern void rh_task_ipset_reg(RHMainServer *ms);
 
-#endif // __RH_TASK_MEMSET_H
+#endif // __RH_TASK_IPSET_H
 
