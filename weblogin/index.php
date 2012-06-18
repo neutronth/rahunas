90c6dd83e797556f29a617ef8c5db9fe1619352f
Neutron Soutmun <neo.neutron@gmail.com>
2012-06-19 00:07:09 +0700

    Redirecting with 302 Move temporary instead of javascript.

--- weblogin/index.php
+++ weblogin/index.php
@@ -41,48 +41,9 @@ $vserver_id = $config["VSERVER_ID"];
 
 $forward_uri  = $config['NAS_LOGIN_PROTO'] . "://" . $config['NAS_LOGIN_HOST'];
 $forward_uri .= !empty($config['NAS_LOGIN_PORT']) ? ":" . $config['NAS_LOGIN_PORT'] : "";
+
+$request_url = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 $forward_uri .= "/login.php?sss=" . time();
+$forward_uri .= "&request_url=" . urlencode ($request_url);
+header ('Location: ' . $forward_uri);
 ?>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<meta http-equiv="Cache-Control" content="no-cache, must-revalidate">
-<meta http-equiv="Pragma" content="no-cache">
-<meta http-equiv="Expires" content="0">
-<title>RahuNAS Authentication</title>
-<style>
-body {
-  font-family: sans-serif;
-  font-size: 10pt;
-}
-a {
-  text-decoration: none;
-}
-
-a:hover {
-  font-weight: bolder;
-  color: #FF0000;
-}
-
-#loadingpic {
-  padding-top: 100px;
-  padding-bottom: 30px;
-}
-</style>
-</head>
-<body>
-<center>
-<div id="loadingpic"><img src="/loading.gif"></div>
-<div><?php echo _("If it is not redirecting within 3 seconds") . "," . _("click"); ?> <a href="<?php echo $forward_uri ?>"><?php echo _("Login Page"); ?></a></div>
-</center>
-
-<script language="JavaScript">
-function redirecting() 
-{
-  redirect_uri = "<?php echo $forward_uri ?>&request_url=" + escape(location);
-  location.replace(redirect_uri);
-}
-setTimeout("redirecting();", 1000);
-</script>
-</body>
-</html>
