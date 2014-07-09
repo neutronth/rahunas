c47a513f3d94503750a34424349f3d4ec4128343
Neutron Soutmun <neo.neutron@gmail.com>
2014-07-09 23:45:20 +0700

    Detect browsers with browscap and add -plain version template support
    [weblogin]
    * weblogin/libs/Broscap.php, weblogin/browscap-cache/{browscap.ini,cache.php}:
      - Add lib and data for browscap - browsers detection.
    * weblogin/rahu_render.class.php:
      - Add browser detection implementation and render -plain template
        on browser that does not support modern features.
    * weblogin/templates/rahunas-bootstrap/*:
      - The template is updated, the bootstrap is updated to version 3.1.0
      - The -plain template is added. Just simple HTML/CSS.
    
    Signed-off-by: Neutron Soutmun <neo.neutron@gmail.com>

--- weblogin/rahu_render.class.php
+++ weblogin/rahu_render.class.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  Copyright (c) 2011, Neutron Soutmun <neutron@rahunas.org>
+  Copyright (c) 2011-2014, Neutron Soutmun <neutron@rahunas.org>
   All rights reserved.
 
   Redistribution and use in source and binary forms, with or without 
@@ -31,6 +31,10 @@
     any other GPL-like (LGPL, GPL2) License.
 */
 
+require_once ("libs/Browscap.php");
+
+use phpbrowscap\Browscap;
+
 class RahuRender {
   private $template;
   private $tplpath;
@@ -40,9 +44,19 @@ class RahuRender {
   function RahuRender ($template) {
     $tpl_ok = false;
 
+    $bc = new Browscap ("browscap-cache/");
+    $bc->doAutoUpdate = false;
+    $browser = $bc->getBrowser ();
+
+    /* Fallback to plain version template for old and unsupported browser */
+    $template_file = $template;
+    if (($browser->Browser == "IE" && $browser->Version < 8) ||
+         !$browser->JavaScript)
+      $template_file = $template . "-plain";
+
     $this->template = $template;
     $this->tplpath = "templates/" . $template . "/";
-    $this->tplfile = $this->tplpath . $template . ".html";
+    $this->tplfile = $this->tplpath . $template_file . ".html";
 
     $handle = @fopen ($this->tplfile, "r");
     $this->buffer = "";
